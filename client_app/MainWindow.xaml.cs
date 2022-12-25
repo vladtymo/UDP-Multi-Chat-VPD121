d9240bea5565b38fae4c82ac7ea5f8165d1b7594
Vlad Tymoshchuk <tymo.vlad@gmail.com>
2022-12-25 15:09:45 +0200

    Add project files.

---
+++ client_app/MainWindow.xaml.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace client_app
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        const string serverIp = "127.0.0.1";
+        const int serverPort = 3344;
+
+        UdpClient client = new UdpClient();
+        IPEndPoint serverEndPoint = new IPEndPoint(IPAddress.Parse(serverIp), serverPort);
+
+        private bool isListening = false;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private async void Listen()
+        {
+            while (isListening)
+            {
+                try
+                {
+                    var response = await client.ReceiveAsync();
+                    string message = Encoding.UTF8.GetString(response.Buffer);
+
+                    chatList.Items.Add(message);
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show(ex.Message);
+                }
+            }
+        }
+
+        private void SendMessage(string message)
+        {
+            try
+            {
+                byte[] data = Encoding.UTF8.GetBytes(message);
+                client.SendAsync(data, data.Length, serverEndPoint);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            
+        }
+        private void JoinMenuClick(object sender, RoutedEventArgs e)
+        {
+            SendMessage("<join>");
+
+            if (!isListening)
+            {
+                isListening = true;
+                Listen();
+            }
+        }
+        private void LeaveMenuClick(object sender, RoutedEventArgs e)
+        {
+            SendMessage("<leave>");
+            isListening = false;
+        }
+        private void SendBtnClick(object sender, RoutedEventArgs e)
+        {
+            string text = msgTextBox.Text;
+
+            if (string.IsNullOrWhiteSpace(text)) return;
+
+            SendMessage(text);
+        }
+
+        private void AboutMenuClick(object sender, RoutedEventArgs e)
+        {
+            MessageBox.Show("Simple multi chat applicaiton using UDP protocol.", "About");
+        }
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            SendMessage("<leave>");
+            isListening = false;
+        }
+    }
+}
---
+++ server_app/Program.cs
@@ -0,0 +1,77 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace server_app
+{
+    public class ChatServer
+    {
+        private UdpClient client;
+        private HashSet<IPEndPoint> members = new HashSet<IPEndPoint>();
+
+        public ChatServer()
+        {
+            client = new UdpClient(3344);
+        }
+
+        public void Join(IPEndPoint endPoint)
+        {
+            members.Add(endPoint);
+        }
+        public void Leave(IPEndPoint endPoint)
+        {
+            members.Remove(endPoint);
+        }
+        public void SendMessage(string message)
+        {
+            byte[] data = Encoding.UTF8.GetBytes(message);
+
+            foreach (var member in members)
+            {
+                client.SendAsync(data, data.Length, member);
+            }
+        }
+
+        public void Start()
+        {
+            IPEndPoint? clientEndPoint = null;
+
+            while (true)
+            {
+                try
+                {
+                    Console.WriteLine("Waiting for a request...");
+                    byte[] request = client.Receive(ref clientEndPoint);
+                    string message = Encoding.UTF8.GetString(request);
+                    Console.WriteLine($"Got a message: {message} from {clientEndPoint}");
+
+                    switch (message)
+                    {
+                        case "<join>":
+                            Join(clientEndPoint);
+                            break;
+                        case "<leave>":
+                            Leave(clientEndPoint);
+                            break;
+                        default:
+                            SendMessage(message);
+                            break;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+            }
+        }
+    }
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            ChatServer server = new ChatServer();
+            server.Start();
+        }
+    }
+}
